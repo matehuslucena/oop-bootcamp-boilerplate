5ef774a9397b22dcd116bc0472e88bc67d1d333e
Matheus Lucena <matheus.lucena@bp.com>
2023-02-06 11:53:48 +0100

    Remove get methods to improve encapsulate

--- src/test/java/oop/RectangleTest.java
+++ src/test/java/oop/RectangleTest.java
@@ -8,17 +8,13 @@ import static org.testng.AssertJUnit.assertEquals;
 public class RectangleTest {
 
     Rectangle rectangle;
+
     @BeforeTest
     public void Setup() {
         double sideA = 2.0;
         double sideB = 3.0;
 
         rectangle = new Rectangle(sideA, sideB);
-    }
-    @Test
-    public void itShouldCreateRectangle() {
-        assertEquals(rectangle.getSideA(), 2.0);
-        assertEquals(rectangle.getSideB(), 3.0);
     }
 
     @Test
