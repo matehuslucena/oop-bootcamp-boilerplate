f1c51c36df8a81fde7ec6d5c2032f9c4ee6be68b
noelia.quiles <noelia.quiles@thoughtworks.com>
2023-02-06 13:44:28 +0100

    Add method productOfTwoChances

--- src/main/java/oop/ProbabilityCalculator.java
+++ src/main/java/oop/ProbabilityCalculator.java
@@ -5,7 +5,11 @@ public class ProbabilityCalculator {
         return (float) 1/6;
     }
 
-    public static float probabilityOfNotGettingSix() {
+    /*public static float probabilityOfNotGettingSix() {
         return (float) 5/6;
+    }*/
+
+    public static float productOfTwoChances(float chanceA, float chanceB) {
+        return chanceA * chanceB;
     }
 }
