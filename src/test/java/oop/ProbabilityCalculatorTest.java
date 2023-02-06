bdec3ed0eee37779039ce8c7afe1f49b8f8d9c7a
Matheus Lucena <matheus.lucena@bp.com>
2023-02-06 13:14:21 +0100

    Add a delta in probability tests

--- src/test/java/oop/ProbabilityCalculatorTest.java
+++ src/test/java/oop/ProbabilityCalculatorTest.java
@@ -7,11 +7,11 @@ import static org.testng.AssertJUnit.assertEquals;
 public class ProbabilityCalculatorTest {
     @Test
     public void itShouldReturnTheChanceOfGetting6WhenRollDice() {
-        assertEquals(ProbabilityCalculator.probabilityOfGettingSix(), (float) 0.16666667);
+        assertEquals(ProbabilityCalculator.probabilityOfGettingSix(), (float) 0.16, 0.01d);
     }
-    
+
     @Test
     public void itShouldReturnTheChanceOfNotGetting6WhenRollDice() {
-        assertEquals(ProbabilityCalculator.probabilityOfNotGettingSix(), (float) 0.8333333);
+        assertEquals(ProbabilityCalculator.probabilityOfNotGettingSix(), (float) 0.83, 0.01d);
     }
 }
